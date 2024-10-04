b20deb2b27fd6632b0fb0c32a7fb4c6199c7eb33
debruine <debruine@gmail.com>
2024-10-04 10:12:40 +0100

    PDFs and updates

--- R/pdf.R
+++ R/pdf.R
@@ -1,9 +1,11 @@
 guides <- list.files(pattern = "guide-\\d+.*\\.qmd")
 
 for (guide in guides) {
-  #quarto::quarto_render(guide, output_format="pdf");
+  #guide <- "case-study-bristol.qmd"
+  quarto::quarto_render(guide, output_format="pdf");
   file.rename(
     paste0("docs/", sub("\\.qmd", "\\.pdf", guide)),
     paste0("resources/", sub("\\.qmd", "\\.pdf", guide))
   )
 }
+
