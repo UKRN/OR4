00cc325e06a2faf0112a2c6dfe819d494b0520e8
Lisa DeBruine <debruine@gmail.com>
2024-10-10 11:58:40 +0100

    PDFs with logo

--- R/pdf.R
+++ R/pdf.R
@@ -1,18 +1,13 @@
-# pages <- list.files(pattern = "(case-study|guide)-.*\\.qmd") |>
-#   setdiff("guide-contents.qmd")
-#
-# for (page in pages) {
-#   quarto::quarto_render(page, output_format = "pdf", metadata_file = "_quarto-guide.yml")
-#   pdf <- sub("\\.qmd", "\\.pdf", page)
-#   file.rename(
-#     file.path("docs", pdf),
-#     file.path("resources", pdf)
-#   )
-# }
+pages <- list.files(pattern = "(case-study|guide)-.*\\.qmd") |>
+  setdiff("guide-contents.qmd")
 
-quarto::quarto_render(output_format = "pdf", profile = "guide")
-pdfs <- list.files("docs", "\\.pdf", full.names = TRUE)
-
-for (pdf in pdfs) {
-  file.rename(pdf, gsub("docs/", "resources/", pdf))
+for (page in pages) {
+  quarto::quarto_render(page, output_format = "pdf", profile = "guide")
 }
+
+# quarto::quarto_render(output_format = "pdf", profile = "guide")
+# pdfs <- list.files("docs", "\\.pdf", full.names = TRUE)
+#
+# for (pdf in pdfs) {
+#   file.rename(pdf, gsub("docs/", "resources/", pdf))
+# }
