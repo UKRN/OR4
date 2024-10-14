5aed7a0d19193de4ec91b03c35f12da20b5fa18c
Lisa DeBruine <debruine@gmail.com>
2024-10-14 14:19:10 +0100

    pdfs

--- R/pdf.R
+++ R/pdf.R
@@ -1,13 +1,8 @@
-pages <- list.files(pattern = "(case-study|guide)-.*\\.qmd") |>
-  setdiff("guide-contents.qmd")
-
-for (page in pages) {
-  quarto::quarto_render(page, output_format = "pdf", profile = "guide")
-}
-
-# quarto::quarto_render(output_format = "pdf", profile = "guide")
-# pdfs <- list.files("docs", "\\.pdf", full.names = TRUE)
+# pages <- list.files(pattern = "(case-study|guide)-.*\\.qmd") |>
+#   setdiff("guide-contents.qmd")
 #
-# for (pdf in pdfs) {
-#   file.rename(pdf, gsub("docs/", "resources/", pdf))
+# for (page in pages) {
+#   quarto::quarto_render(page, output_format = "pdf", profile = "guide")
 # }
+
+quarto::quarto_render(output_format = "pdf", profile = "guide")
