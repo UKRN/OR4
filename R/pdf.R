f620633d238dea2d69e1aa4642ab399342b33b8f
Lisa DeBruine <debruine@gmail.com>
2024-10-15 15:29:19 +0100

    Custom headers and footers for case studies

--- R/pdf.R
+++ R/pdf.R
@@ -1,15 +1,22 @@
-# individual pages
-pages <- list.files(pattern = "(case-study|guide)-.*\\.qmd") |>
+# individual guide pages
+pages <- list.files(pattern = "guide-.*\\.qmd") |>
   setdiff("guide-contents.qmd")
 
 for (page in pages) {
-  quarto::quarto_render(page, output_format = "pdf", profile = "pages")
+  quarto::quarto_render(page, output_format = "pdf", profile = "guide")
+}
+
+# individual case study pages
+pages <- list.files(pattern = "case-study-.*\\.qmd")
+
+for (page in pages) {
+  quarto::quarto_render(page, output_format = "pdf", profile = "cs")
 }
 
 
 # full guide
 file.rename("guide/index.md", "index.md")
 quarto::quarto_render(output_format = "pdf",
-                      profile = "guide",
+                      profile = "guidebook",
                       output_file = "or4-guide.pdf")
 file.rename("index.md", "guide/index.md")
